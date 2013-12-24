d69cf7ba6d513d19a2209cdb06862fc2c0ae9f16
John McLear <john@mclear.co.uk>
2013-12-24 03:36:24 +0000

    much smarter

--- static/js/clear_formatting.js
+++ static/js/clear_formatting.js
@@ -1,20 +1,18 @@
 exports.postAceInit = function(hook, context){
   $('#clearFormatting').click(function(){
     context.ace.callWithAce(function(ace){
-      var rep = ace.ace_getRep();
+
+      var rep = ace.ace_getRep(); // get the current user selection
       var isSelection = (rep.selStart[0] !== rep.selEnd[0] || rep.selStart[1] !== rep.selEnd[1]);
       if(!isSelection) return false; // No point proceeding if no selection..
 
-      // note this is just bodged together, we need to query the documentattributemanager probably to find out 
-      // which attributes exist...
-      ace.ace_setAttributeOnSelection("bold", false);
-      ace.ace_setAttributeOnSelection("italic", false);
-      ace.ace_setAttributeOnSelection("underline", false);
-      ace.ace_setAttributeOnSelection("strikethrough", false);
-      ace.ace_setAttributeOnSelection("fontcolor", false);
-      ace.ace_setAttributeOnSelection("color", false);
-      ace.ace_setAttributeOnSelection("subscript", false);
-      ace.ace_setAttributeOnSelection("superscript", false);
+      var attrs = rep.apool.attribToNum; // get the attributes on this document
+      $.each(attrs, function(k, v){ // for each attribute
+        var attr = k.split(",")[0]; // get the name of the attribute
+        if(attr !== "author"){ // if its not an author attribute
+          ace.ace_setAttributeOnSelection(attr, false); // set the attribute to false
+        }
+      });
     },'clearFormatting' , true);
   });
 }
