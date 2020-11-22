3b62983e20f754aabdac249d336bc57bad15cb1e
John McLear <john@mclear.co.uk>
2020-11-22 15:13:09 +0000

    autofixes from Etherpad checkPlugins.js

--- clear_formatting.js
+++ clear_formatting.js
@@ -1,9 +1,8 @@
-var eejs = require('ep_etherpad-lite/node/eejs/');
-var settings = require('ep_etherpad-lite/node/utils/Settings');
-var checked_state = '';
+const eejs = require('ep_etherpad-lite/node/eejs/');
+const settings = require('ep_etherpad-lite/node/utils/Settings');
+const checked_state = '';
 
 exports.eejsBlock_dd_format = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require('ep_clear_formatting/templates/clear_formatting_menu.ejs');
+  args.content += eejs.require('ep_clear_formatting/templates/clear_formatting_menu.ejs');
   return cb();
-}
-
+};
--- static/js/clear_formatting.js
+++ static/js/clear_formatting.js
@@ -1,18 +1,17 @@
-exports.postAceInit = function(hook, context){
-  $('#clearFormatting').click(function(){
-    context.ace.callWithAce(function(ace){
+exports.postAceInit = function (hook, context) {
+  $('#clearFormatting').click(() => {
+    context.ace.callWithAce((ace) => {
+      const rep = ace.ace_getRep(); // get the current user selection
+      const isSelection = (rep.selStart[0] !== rep.selEnd[0] || rep.selStart[1] !== rep.selEnd[1]);
+      if (!isSelection) return false; // No point proceeding if no selection..
 
-      var rep = ace.ace_getRep(); // get the current user selection
-      var isSelection = (rep.selStart[0] !== rep.selEnd[0] || rep.selStart[1] !== rep.selEnd[1]);
-      if(!isSelection) return false; // No point proceeding if no selection..
-
-      var attrs = rep.apool.attribToNum; // get the attributes on this document
-      $.each(attrs, function(k, v){ // for each attribute
-        var attr = k.split(",")[0]; // get the name of the attribute
-        if(attr !== "author"){ // if its not an author attribute
+      const attrs = rep.apool.attribToNum; // get the attributes on this document
+      $.each(attrs, (k, v) => { // for each attribute
+        const attr = k.split(',')[0]; // get the name of the attribute
+        if (attr !== 'author') { // if its not an author attribute
           ace.ace_setAttributeOnSelection(attr, false); // set the attribute to false
         }
       });
-    },'clearFormatting' , true);
+    }, 'clearFormatting', true);
   });
-}
+};
